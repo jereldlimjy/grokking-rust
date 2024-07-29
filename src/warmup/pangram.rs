d10206915cd3001874fc37801b25467b3bebbf50
jereldlimjy <jereldlimjy@hotmail.com>
2024-07-29 21:19:47 +0800

    add pangram qn

---
+++ src/warmup/pangram.rs
@@ -0,0 +1,52 @@
+use std::collections::HashSet;
+
+/// Checks if the given sentence is a pangram.
+/// A pangram is a sentence that contains every letter of the alphabet at least once.
+///
+/// # Arguments
+///
+/// * `sentence` - A string slice that represents the sentence to be checked.
+///
+/// # Returns
+///
+/// * `true` if the sentence is a pangram.
+/// * `false` otherwise.
+pub fn check_if_pangram(sentence: String) -> bool {
+    let mut chars = HashSet::new();
+
+    for chr in sentence.to_ascii_lowercase().chars() {
+        if chr.is_ascii_lowercase() {
+            chars.insert(chr);
+        }
+    }
+
+    chars.len() == 26
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_pangram() {
+        assert_eq!(
+            check_if_pangram("TheQuickBrownFoxJumpsOverTheLazyDog".to_string()),
+            true
+        );
+    }
+
+    #[test]
+    fn is_not_pangram() {
+        assert_eq!(check_if_pangram("This is not a pangram".to_string()), false);
+    }
+
+    #[test]
+    fn is_not_pangram_digits() {
+        assert_eq!(
+            check_if_pangram(
+                "This is not a pangram. Here is a random number: 123456789".to_string()
+            ),
+            false
+        );
+    }
+}
